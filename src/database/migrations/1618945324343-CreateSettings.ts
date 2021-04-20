a32875d2ba7f824143ad014848c61a22aec9fcff
Willian Komada <willian10komada@hotmail.com>
2021-04-20 17:06:14 -0300

    database migrations entities repositories routes  controllers

---
+++ src/database/migrations/1618945324343-CreateSettings.ts
@@ -0,0 +1,45 @@
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
+
+export class CreateSettings1618945324343 implements MigrationInterface {
+
+    // yarn typeorm migration:run
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.createTable(
+            new Table({
+                name: "settings",
+                columns: [
+                    {
+                        name: "id",
+                        type: "uuid",
+                        isPrimary: true
+                    },
+                    {
+                        name: "username",
+                        type: "varchar"
+                    },
+                    {
+                        name: "chat",
+                        type: "boolean",
+                        default: true
+                    },
+                    {
+                        name: "updated_at",
+                        type: "timestamp",
+                        default: "now()"
+                    },
+                    {
+                        name: "created_at",
+                        type: "timestamp",
+                        default: "now()"
+                    }
+                ]
+            })
+        )
+    }
+
+    // yarn typeorm migration:revert
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropTable("settings");
+    }
+
+}
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express';
 
+import './database';
+import { routes } from './routes';
+
 const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => console.log("Server is runing on port 3333"));
 
 /*
 GET = Buscar de informações
@@ -9,16 +18,4 @@ PUT = Alteração de informações
 DELETE = Deletar informações
 PATCH = Alterar uma informação específica
 */
-
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá NLW05"
-  });
-}); 
-
-app.post("/", (request, response) => {
-  return response.json({ message: "Usuário salvo com sucesso!" });
-});
-
-app.listen(3333, () => console.log("Server is runing on port 3333"));
 
