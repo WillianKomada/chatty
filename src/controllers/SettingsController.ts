f8ce1ad80eca8e0bc388c7cda946dafb9b74c5da
Willian Komada <willian10komada@hotmail.com>
2021-04-21 21:18:42 -0300

    astronautas

--- src/controllers/SettingsController.ts
+++ src/controllers/SettingsController.ts
@@ -1,21 +1,21 @@
 import { Request, Response } from 'express';
-import { getCustomRepository } from 'typeorm';
-import { SettingsRepository } from '../repositories/SettingsRepository';
+import { SettingsService } from '../services/SettingsService';
 
 class SettingsController {
   async create(request: Request, response: Response) {
     const { username, chat } = request.body;
 
-    const settingsRepository = getCustomRepository(SettingsRepository);
+    const settingsService = new SettingsService();
 
-    const settings = settingsRepository.create({
-      username,
-      chat
-    })
+    try {
+      const settings = await settingsService.create({ username, chat });
 
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
+      return response.json(settings);
+    } catch(err) {
+      return response.status(400).json({ 
+        message: err.message,
+      });
+    }
   }
 }
 
--- src/database/migrations/1619043962788-CreateUsers.ts
+++ src/database/migrations/1619043962788-CreateUsers.ts
@@ -1,12 +1,11 @@
 import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export class CreateSettings1618945324343 implements MigrationInterface {
+export class CreateUsers1619043962788 implements MigrationInterface {
 
-    // yarn typeorm migration:run
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: "settings",
+                name: "users",
                 columns: [
                     {
                         name: "id",
@@ -14,32 +13,21 @@ export class CreateSettings1618945324343 implements MigrationInterface {
                         isPrimary: true
                     },
                     {
-                        name: "username",
+                        name: "email",
                         type: "varchar"
-                    },
-                    {
-                        name: "chat",
-                        type: "boolean",
-                        default: true
-                    },
-                    {
-                        name: "updated_at",
-                        type: "timestamp",
-                        default: "now()"
                     },
                     {
                         name: "created_at",
                         type: "timestamp",
                         default: "now()"
-                    }
+                    },
                 ]
             })
         );
     }
 
-    // yarn typeorm migration:revert
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable("settings");
+        await queryRunner.dropTable("users");
     }
 
 }
